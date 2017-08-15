6399bde5f82fc081b517917e61ad1cee256c0678
Jacob Peck <gatesphere@gmail.com>
2017-08-15 15:34:37 -0400

    all game logic complete

--- kaalu-sim.py
+++ kaalu-sim.py
@@ -48,6 +48,8 @@ class Game:
                    'f': (2,3,2),
                    'g': (2,3,2) }
     
+    self.available_piles = ['a'] # as more piles get used, their letters are appended -- for homomorphism
+    
     self.stock_black = 7
     self.stock_white = 7
     
@@ -55,6 +57,8 @@ class Game:
     self.primacy_white = 0
     self.primacy_neutral = 0
     self.check_primicies()
+    
+    self.game_log = []
   #@+node:peckj.20170814142235.1: *3* check_primicies
   def check_primicies(self):
     d = {'black': 0,
@@ -99,12 +103,26 @@ class Game:
       
   #@+node:peckj.20170814144023.1: *3* display_board
   def display_board(self):
-    maxpad = max(sorted(self.board.values(), key=lambda x: sorted(x, reverse=True), reverse=True)[0])+1
+    maxpad_b = max([x[0] for x in self.board.values()])
+    maxpad_n = max([x[1] for x in self.board.values()])
+    maxpad_w = max([x[2] for x in self.board.values()])
+    #maxpad = max(sorted(self.board.values(), key=lambda x: sorted(x, reverse=True), reverse=True)[0])+1
     for key,pile in sorted(self.board.items()):
       b = pile[0] * CHAR_BLACK
       n = pile[1] * CHAR_NEUTRAL
       w = pile[2] * CHAR_WHITE
-      print '%s  %s%s%s' % (key, b.center(maxpad), n.center(maxpad), w.center(maxpad))
+      print '%s: %s %s %s' % (key, b.rjust(maxpad_b), n.center(maxpad_n), w.ljust(maxpad_w))
+  #@+node:peckj.20170815152055.1: *3* print_log
+  def print_log(self):
+    if len(self.game_log) % 2 == 1:
+      self.game_log.append('')
+    
+    pairs = [self.game_log[i:i+2] for i in xrange(0, len(self.game_log), 2)]
+    print pairs
+    print 'Game log:'
+    for idx,pair in enumerate(pairs):
+      print '%s. %s %s' % (idx+1, str(pair[0]).ljust(5), str(pair[1]).ljust(5))
+      
   #@+node:peckj.20170814150015.1: *3* valid_move
   def valid_move(self, move):
     # first pass -- ensure the move is syntactically valid
@@ -112,6 +130,11 @@ class Game:
     if move_syntax.search(m) is None:
       print 'does not match syntax'
       return False
+    piles = filter(str.isalpha, move)
+    for p in piles:
+      if p not in self.available_piles:
+        print 'move not homomorphic'
+        return False
     # if it passes, parse it niavely, returning a new hypothetical gamestate
     new_gamestate = self.parse_move(m)
     if new_gamestate is False:
@@ -177,7 +200,31 @@ class Game:
     # at least 7 stones
     # at least one of each color
     # no three-way ties
-    print "game.check_board_validity stubbed."
+    count_black = gamestate['stock_black']
+    count_white = gamestate['stock_white']
+    count_neutral = 0
+    
+    for key,pile in gamestate['board'].items():
+      count_black += pile[0]
+      count_neutral += pile[1]
+      count_white += pile[2]
+      # check for three-way ties
+      if pile.count(pile[0]) == 3:
+        print "three way tie in pile %s" % key
+        return False
+      # check for at least 7 stones
+      if sum(pile) < 7:
+        print "fewer than seven stones in pile %s" % key
+        return False
+      # check for at least one of each color
+      if pile[0] < 1 or pile[1] < 1 or pile[2] < 1:
+        print "missing stones in pile %s" % key
+        return False
+    
+    if count_black != 21 or count_white != 21 or count_neutral != 21:
+      print 'stones created or destroyed'
+      return False
+    
     return True
   #@+node:peckj.20170814155217.1: *4* check_for_undo
   def check_for_undo(self, new_gamestate):
@@ -187,17 +234,34 @@ class Game:
       return False # first move of the game, so it's valid by default
     return old_gamestate == new_gamestate
   #@+node:peckj.20170814155618.1: *3* execute_move
-  def execute_move(self, new_gamestate):
+  def execute_move(self, move, new_gamestate):
+    move = move.strip().lower()
+    
     self.previous_gamestate = self.construct_gamestate()
     self.stock_black = new_gamestate['stock_black']
     self.stock_white = new_gamestate['stock_white']
     self.board = new_gamestate['board']
     
+    newest_pile = sorted(filter(str.isalpha, move))[-1]
+    if newest_pile == self.available_piles[-1] and newest_pile != 'g':
+      self.available_piles.append(chr(ord(newest_pile)+1))
+    print self.available_piles
+    
     self.check_primicies()
     if self.active_player == self.player_black:
+      if self.over() and self.primicy_black >= PILES_TO_WIN:
+        move = move + '#'
+      elif self.over() and self.primicy_white >= PILES_TO_WIN:
+        move = move + '^'
       self.active_player = self.player_white
     else:
+      if self.over() and self.primicy_white >= PILES_TO_WIN:
+        move = move + '#'
+      elif self.over() and self.primicy_black >= PILES_TO_WIN:
+        move = move + '^'
       self.active_player = self.player_black
+    
+    self.game_log.append(move)
   #@-others
 #@+node:peckj.20170814140850.1: ** main
 def main():
@@ -219,7 +283,7 @@ def main():
     m = game.valid_move(move)
     if m:
       print m  
-      game.execute_move(m)
+      game.execute_move(move, m)
     else:
       print "Invalid move."
   
